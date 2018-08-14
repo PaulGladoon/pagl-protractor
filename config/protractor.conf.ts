83ab98ce58be309895d0d102dfaaf27c9ab76917
Your Name <administrator@Administrators-MacBook-Pro-3.local>
2018-08-14 21:42:25 +0300

    add test

---
+++ config/protractor.conf.ts
@@ -0,0 +1,29 @@
+/* tslint:disable:object-literal-sort-keys */
+import { browser, Config } from 'protractor'
+import { protractorConfigBuild } from './config.build'
+import * as remote from 'selenium-webdriver/remote'
+import path = require('path')
+
+const conf: Config = {
+  ...protractorConfigBuild(),
+  // directConnect: true,
+  // Connecting directly to ChromeDriverServer
+  framework: 'mocha',
+  // restartBrowserBetweenTests: true,
+  allScriptsTimeout: 30 * 1000,
+  // seleniumAddress: 'http://test:test-password@localhost:4446/wd/hub', // ggr
+  seleniumAddress: 'http://localhost:4444/wd/hub', // local
+  // Needed to make async/await work. Disables control flow.
+  SELENIUM_PROMISE_MANAGER: false,
+
+  onPrepare: async () => {
+    await browser.waitForAngularEnabled(false)
+    const caps = await browser.getCapabilities()
+    const browserName = caps.get('browserName')
+    browser.___browserName = browserName
+    await browser.manage().window().maximize()
+    await browser.setFileDetector(new remote.FileDetector())
+  }
+}
+
+exports.config = conf
---
+++ specs/test.spec.ts
@@ -0,0 +1,54 @@
+import { browser, $$, $ } from 'protractor'
+
+describe('TEST DESCRIBE', () => {
+
+  beforeEach(async () => {
+    await browser.get('/')
+  })
+
+  it('TEST IT', async () => {
+
+    const userName = 'Pavel'
+    const userPassword = '12345'
+    const tableHeadElements = $$('#app > div > div > div:nth-child(2) > table:nth-child(8) > thead > tr > td')
+    const tableRowsElements = $$('.text-center > tbody > tr')
+
+    await $$('.form-control').get(0).sendKeys(userName)
+    await $$('.form-control').get(1).sendKeys(userPassword)
+    await $('.btn-primary').click()
+
+    async function getTableData() {
+      const rowsCount = await tableRowsElements.count()
+      const rowsList = []
+
+      const tableHeaderList = await tableHeadElements.reduce(async function(acc, elem) {
+        const headerText = await elem.getText()
+        acc.push(headerText)
+        return acc
+      }, [])
+
+      for (let i = 0; i < rowsCount; i++) {
+        const rowData = await tableRowsElements.get(i).$$('td').reduce(async function(acc, elem) {
+          const columnData = await elem.getText()
+          acc.push(columnData)
+          return acc
+        }, [])
+        rowsList.push(rowData)
+      }
+
+      const result = rowsList.reduce(function(acc, row) {
+        const data = row.reduce(function(acc, elem, index) {
+          acc[tableHeaderList[index]] = elem
+          return acc
+        }, {})
+        acc.push(data) 
+        return acc
+      }, [])
+
+      console.log(result)
+    }
+
+    await getTableData()
+  })
+
+})
